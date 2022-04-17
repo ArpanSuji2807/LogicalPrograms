4b6244702ccc944173ddbdd01d551b0180735d2a
Praan26 <arpanap102@gmail.com>
2022-04-17 13:39:59 +0530

    [Arpan] Add: Fibonacci series added

---
+++ LogicalPrograms/LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        int num1 = 0, num2 = 1, num3 = 0;
+        public int Series()
+        {
+            Console.WriteLine("Enter the number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                num3 = num1 + num2;
+                num1 = num2;
+                num2 = num3;
+                Console.WriteLine(num1);
+            }
+            return num1;
+        }
+    }
+}
--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LogicalPrograms;
+Console.WriteLine("Hello welcome to my program");
+bool end = true;
+Console.WriteLine("1.Fibonacci Series");
+while (end)
+{
+    Console.WriteLine("Choose an option");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1: 
+            FibonacciSeries series = new FibonacciSeries();
+            series.Series();
+            break;
+        case 2:
+            Console.WriteLine("Enter a valid choice");
+            end = false;
+            break;
+        default:
+            break;
+    }
+}
